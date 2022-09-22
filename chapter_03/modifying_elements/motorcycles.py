7fa93e88dc1661ecb57b3c6f3fb0966d61657a0a
Jonathan Barker <jonathan.barker09@gmail.com>
2022-09-22 17:17:20 +0000

    modified code to include remove statement and print sentence

--- chapter_03/modifying_elements/motorcycles.py
+++ chapter_03/modifying_elements/motorcycles.py
@@ -1,5 +1,7 @@
 motorcyles = ['honda', 'yamaha', 'suzuki', 'ducati']
 print(motorcyles)
 
-motorcyles.remove('ducati')
+too_expensive = 'ducati'
+motorcyles.remove(too_expensive)
 print(motorcyles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
