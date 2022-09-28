165a99b0b14b71f3894acadd1b67410117f97546
Jonathan Barker <jonathan.barker09@gmail.com>
2022-09-28 16:42:51 +0000

    reverted code to use the sorted function

--- chapter_03/organizing_lists/cars.py
+++ chapter_03/organizing_lists/cars.py
@@ -3,12 +3,8 @@ cars = ['bmw', 'audi', 'toyota', 'subaru']
 print("Here is the original list:")
 print(cars)
 
-print("\nHere is the sort function:")
-cars.sort()
-print(cars)
-
-#print("\nHere is the sorted list:")
-#print(sorted(cars))
+print("\nHere is the sorted list:")
+print(sorted(cars))
 
 print("\nHere is the original list again:")
 print(cars)
