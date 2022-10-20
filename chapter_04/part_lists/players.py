6fd319eda33a72943b6b60152d09ed24351267b6
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-20 15:50:22 +0000

    changed code to loop through first 3 names

--- chapter_04/part_lists/players.py
+++ chapter_04/part_lists/players.py
@@ -1,2 +1,5 @@
 players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[:4])
+
+print("Here are the first three players on my team:\n")
+for player in players[:3]:
+    print(player.title())
