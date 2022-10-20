68ba6f776ff75fe6514ec8cba5a17edeac359f2a
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-20 16:34:15 +0000

    added code to append both lists

--- chapter_04/part_lists/foods.py
+++ chapter_04/part_lists/foods.py
@@ -1,6 +1,9 @@
 my_foods = ['pizza', 'falafel', 'carrot cake']
 friend_foods = my_foods[:]
 
+my_foods.append('canoli')
+friend_foods.append('ice cream')
+
 print("My favorite foods are: ")
 print(my_foods)
 
