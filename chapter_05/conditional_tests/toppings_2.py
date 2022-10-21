3385b3ef8fa3f06fe0e39d0c502550b067306e8e
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-21 17:05:53 +0000

    created python code to mess around with input functions and lists using the idea of toppings

---
+++ chapter_05/conditional_tests/toppings_2.py
@@ -0,0 +1,7 @@
+# This is to mess around with input functions and lists
+favorite_toppings = []
+
+choice = input("What is your favorite pizza topping? [Type anything below.]\n")
+favorite_toppings.append(choice)
+
+print("Your favorite toppings are: \n" + str(favorite_toppings))
