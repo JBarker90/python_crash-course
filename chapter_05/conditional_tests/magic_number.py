d04ad815aa53f4cb3c15ac35c5eee6d35c73d913
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-25 16:59:19 +0000

    modified code to include input function from user

--- chapter_05/conditional_tests/magic_number.py
+++ chapter_05/conditional_tests/magic_number.py
@@ -1,4 +1,6 @@
-answer = 17
+answer = input("I am thinking of a number between 1 and 100. What is it?\nYour answer: ")
 
-if answer != 42:
+if answer == 42:
+    print("Congratulations! You guessed the answer!!!")
+else:
     print("That is not the correct answer. Please try again!")
