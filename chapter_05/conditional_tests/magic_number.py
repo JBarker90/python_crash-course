e0646b245bc0c94a599d3530b24c1b8cbe1f911c
Jonathan Barker <jonathan.barker09@gmail.com>
2022-10-25 17:00:12 +0000

    fixed logic error

--- chapter_05/conditional_tests/magic_number.py
+++ chapter_05/conditional_tests/magic_number.py
@@ -1,4 +1,4 @@
-answer = input("I am thinking of a number between 1 and 100. What is it?\nYour answer: ")
+answer = int(input("I am thinking of a number between 1 and 100. What is it?\nYour answer: "))
 
 if answer == 42:
     print("Congratulations! You guessed the answer!!!")
